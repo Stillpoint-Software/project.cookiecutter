45afd15fda903f67f445259179ea0964904b9eb8
annette.findley <annette.varndell@wagglebee.net>
2025-07-07 08:49:22 -0400

    fixed docker for MongoDb

--- docker-project/{{cookiecutter.assembly_name}}/src/{{cookiecutter.assembly_name}}.Api/Infrastructure/AuditSetup.cs
+++ docker-project/{{cookiecutter.assembly_name}}/src/{{cookiecutter.assembly_name}}.Api/Infrastructure/AuditSetup.cs
@@ -21,7 +21,7 @@ public static class AuditSetup
 
     public static void ConfigureAudit(WebApplicationBuilder builder)
     {
-        var connectionString = builder.Configuration.GetConnectionString("{{cookiecutter.databaseName}}");
+        var connectionString = builder.Configuration.GetConnectionString("{{cookiecutter.database_name}}");
 
         var optionsBuilder = new DbContextOptionsBuilder<SampleContext>();
         {% if cookiecutter.database == "PostgreSql" %}
